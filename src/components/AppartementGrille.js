84d4c2d15a47a392cddd6f88f95e911b7027e02d
Julie Philippe <philippejuliepro@gmail.com>
2024-10-22 15:30:04 +0200

    Visuel de la grille appartement

--- src/components/AppartementGrille.js
+++ src/components/AppartementGrille.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./AppartementGrille.css";
-import Appartement from "../components/Appartement";
+import { NavLink } from "react-router-dom";
 
 function AppartementGrille() {
+  const [appartements, setAppartements] = useState([]);
+
+  useEffect(() => {
+    // Appel à l'API pour récupérer les données
+    fetch("http://localhost:8080/api/properties")
+      .then((response) => response.json())
+      .then((data) => {
+        setAppartements(data); // Stocke la liste des appartements
+      })
+      .catch((error) => {
+        console.error("Erreur lors de la récupération des données:", error);
+      });
+  }, []);
+
   return (
     <div className="Grille">
-      <Appartement />
+      {appartements.map((appartement) => (
+        <NavLink
+          key={appartement.id}
+          className="card"
+          to={`/logement/${appartement.id}`}
+        >
+          <div className="card-content">
+            <img
+              src={appartement.cover}
+              alt={appartement.title}
+              className="cover-image"
+            />
+            <div className="card-title">{appartement.title}</div>
+          </div>
+        </NavLink>
+      ))}
     </div>
   );
 }
